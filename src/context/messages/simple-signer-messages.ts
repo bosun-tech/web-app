2bdbd8517998d7790aa2e2e096429cd4a3819791
bosuntechnologies <bosuntechnologies@gmail.com>
2024-11-03 10:25:37 +0100

    Add Simple Signer popup messages

---
+++ src/context/messages/simple-signer-messages.ts
@@ -0,0 +1,7 @@
+export const CONNECTION_CANCELLED =
+	'Wallet connection was canceled by the user';
+export const UNKNOWN_WALLET_CONNECTION_ERROR =
+	'Unknown error when connecting to your wallet';
+export const CONNECTION_SUCCESSFUL = 'Successfully connected to your wallet';
+export const LOGOUT_SUCCESSFUL = 'Logout successful';
+export const SERVICE_UNAVAILABLE = 'Service unavailable';
