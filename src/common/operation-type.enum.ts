1718c470a1bb0b60ff6b364d757be088451fd26f
bosuntechnologies <bosuntechnologies@gmail.com>
2025-03-13 12:47:10 +0100

    Feature/withdraw (#6)
    * Remove DepositInstructions and PlatformDeposit components from the platform directory.
    
    * Implement withdraw flow in platform.spec.ts, including tests for form visibility and validation errors.
    
    * Add Anclap UI constants and Anchor configuration to constants.ts, including logos and interface options.
    
    * Add OperationType enum for deposit and withdraw operations in operation-type.enum.ts
    
    * Add OperationInstructions component to display transaction details and instructions for deposit and withdrawal operations.
    
    * Update routing to replace PlatformDeposit with PlatformOperationForm for handling various platform operations.
    
    * Update PlatformAuthorization to use search parameters for dynamic routing based on operation type.
    
    * Add PlatformForm component for selecting country, anchor, and operation with navigation support.
    
    * Add PlatformOperationForm component for handling deposit and withdrawal operations with user input validation and dynamic interface selection.

---
+++ src/common/operation-type.enum.ts
@@ -0,0 +1,4 @@
+export enum OperationType {
+	DEPOSIT = 'Deposit',
+	WITHDRAW = 'Withdraw',
+}
--- src/components/platform/OperationInstructions.tsx
+++ src/components/platform/OperationInstructions.tsx
@@ -1,4 +1,6 @@
-interface DepositInstructionsProps {
+import { OperationType } from '@/common/operation-type.enum';
+
+interface OperationInstructionsProps {
 	transactionId: string;
 	estimatedTime: string;
 	amount: number;
@@ -6,23 +8,30 @@ interface DepositInstructionsProps {
 		accountNumber: string;
 		bankName: string;
 	};
+	operationType: OperationType;
 }
 
-export default function DepositInstructions({
+export default function OperationInstructions({
 	transactionId,
 	estimatedTime,
 	amount,
 	bankInfo,
-}: DepositInstructionsProps) {
+	operationType,
+}: OperationInstructionsProps) {
 	return (
-		<div className="flex flex-col gap-6" data-testid="deposit-instructions">
-			<h2 className="text-3xl font-bold" data-testid="deposit-title">
-				Deposit
+		<div className="flex flex-col gap-6" data-testid="operation-instructions">
+			<h2 className="text-3xl font-bold" data-testid="operation-title">
+				{operationType === OperationType.DEPOSIT
+					? OperationType.DEPOSIT
+					: OperationType.WITHDRAW}
 			</h2>
 
-			<div className="flex flex-col gap-6">
+			<div className="flex flex-col gap-6 rounded-2xl shadow-lg p-6">
 				<h3 className="text-2xl font-bold" data-testid="instructions-title">
-					Deposit Instructions
+					{operationType === OperationType.DEPOSIT
+						? OperationType.DEPOSIT
+						: OperationType.WITHDRAW}{' '}
+					Instructions
 				</h3>
 
 				<div className="flex flex-col gap-4">
